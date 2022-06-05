0e0930c845f648a77fd8442222449a1915fb587e
Connie Leung <cleung334@gmail.com>
2022-06-05 18:04:34 +0800

    feat: add npm script for cspell

--- schematics/nest-add/helpers/cspell.helper.ts
+++ schematics/nest-add/helpers/cspell.helper.ts
@@ -1,4 +1,4 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { Rule, SchematicContext, SchematicsException, Tree } from "@angular-devkit/schematics";
 import { addPackageJsonDependency } from "@schematics/angular/utility/dependencies";
 import { cspell } from "../constants";
 import { Schema } from "../schema";
@@ -8,14 +8,15 @@ export function addCspell(options: Schema): Rule {
       if (options.isAddCspell) {
         addPackageJsonDependency(tree, cspell);
         context.logger.info(`Added ${cspell.name}@${cspell.version}`);
-        copyCspellJson(tree, context)
+        createCspellJson(tree, context);
+        addNpmScript(tree, context);
       }
   
       return tree
     }
 }
 
-function copyCspellJson(tree: Tree, context: SchematicContext) {    
+function createCspellJson(tree: Tree, context: SchematicContext) {    
     const content = {
         "version": "0.2",
         "language": "en",
@@ -23,8 +24,23 @@ function copyCspellJson(tree: Tree, context: SchematicContext) {
         "flagWords": ["hte"]
     };
 
-    tree.create('cspell.json', JSON.stringify(content, null, 2)
-    );
+    tree.create('cspell.json', JSON.stringify(content, null, 2));
     context.logger.info('Added cspell.json')
     return true
+}
+
+function addNpmScript(tree: Tree, context: SchematicContext) {
+  const pkgPath = 'package.json' 
+  const buffer = tree.read(pkgPath)
+
+  if (buffer === null) {
+    throw new SchematicsException(`Cannot find ${pkgPath}`);
+  }
+
+  const packageJson = JSON.parse(buffer.toString());
+  packageJson.scripts.cspell = "cspell --no-must-find-files src/**/*.{ts,js}";
+  tree.overwrite(pkgPath, JSON.stringify(packageJson, null, 2));
+  context.logger.info('Added cspell script in package.json')
+
+  return tree;
 }
