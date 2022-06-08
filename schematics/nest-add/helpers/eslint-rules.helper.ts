02ef9b02169ec7944c8b8cf18e52b25bec0ec50a
Connie Leung <cleung334@gmail.com>
2022-06-09 00:40:03 +0800

    refactor: move schematic rules of eslint to eslint-rule.helper.ts

--- schematics/nest-add/helpers/eslint-rules.helper.ts
+++ schematics/nest-add/helpers/eslint-rules.helper.ts
@@ -1,8 +1,19 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
-import { eslintSonarjs } from "../constants";
+import { eslintPluginRxjs, eslintSonarjs } from "../constants";
 import { Schema } from "../schema";
 import { addDependencies } from "./dependency.helper";
 import { updateEslintrc } from "./eslint.helper";
+
+export function addEslintRxjs(options: Schema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    if (options.isAddEslintRxjs) {
+      addDependencies(tree, context, [eslintPluginRxjs]);
+      updateEslintrc(tree, context, { eslintExtend: 'plugin:rxjs/recommended' });
+    }
+
+    return tree
+  }
+}
 
 export function addEslintSonarjs(options: Schema): Rule {
     return (tree: Tree, context: SchematicContext) => {
