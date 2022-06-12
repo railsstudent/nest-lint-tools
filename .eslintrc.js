f059006c5736fe9d821cce2291f0195b49757586
Connie Leung <cleung334@gmail.com>
2022-06-12 23:44:45 +0800

    chore: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,19 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
-    ],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
 
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": './tsconfig.eslint.json',
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "prettier"
-    ],
-    "ignorePatterns": ['.eslintrc.js'],
-    "rules": {
-        "prettier/prettier": ["error", {"singleQuote": true, "trailingComma": "all", "tabWidth": 2, "singleQuote": true, "semi": false, "printWidth": 140 }]
-    }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'all', tabWidth: 2, semi: false, printWidth: 140 }],
+  },
 }
