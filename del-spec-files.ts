10a2b58c3541ecd02f165455ef9a58008971d3b1
Connie Leung <cleung334@gmail.com>
2022-06-14 00:14:45 +0800

    fix: build files in dist/schematics directory

--- del-spec-files.ts
+++
@@ -1,5 +0,0 @@
-import * as del from 'del'
-;(async () => {
-  const deletedFilePaths = await del(['./schematics/**/*.spec.{js,js.map,d.ts}'])
-  console.log('Deleted files:\n', deletedFilePaths.join('\n'))
-})()
